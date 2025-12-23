acb2447eef5699b7ca26581f281e8d4a8a44e082
heinz <heinz@FPC>
2025-12-23 20:30:23 +0100

    Projektdateien hinzufügen.

---
+++ AudioProcessor.cs
@@ -0,0 +1,218 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System.Windows;
+
+namespace CLiveNormalizer;
+
+public sealed class WaveInVolumeProvider : VolumeProvider
+{
+    private readonly WaveInEvent capture;
+
+    public WaveInVolumeProvider(double[] peaks, double[] avgs, float dbcorr, int deviceNr, WaveFormat waveFormat)
+    {
+        _dbCorr = dbcorr;
+        ieeeFloat = waveFormat.Encoding.ToString() == "IeeeFloat";
+
+        window = Application.Current.MainWindow;
+        _peaks = peaks;
+        _avgs = avgs;
+        stPeaks = [];
+        timer = new() { Interval = 1000 };
+        timer.Tick += new EventHandler(Timer_Tick);
+
+        capture = new()
+        {
+            DeviceNumber = deviceNr,
+            WaveFormat = waveFormat
+        };
+
+        capture.DataAvailable += ieeeFloat ? OnDataAvailableFP : OnDataAvailablePCM;
+    }
+
+    public override Task Start()
+    {
+        try
+        {
+            capture.StartRecording();
+        }
+        catch { MessageBox.Show("Error opening WaveIn stream. Check the sample rate and channels!", "Device Error", MessageBoxButton.OK, MessageBoxImage.Error); }
+
+        timer.Start();
+
+        return Task.CompletedTask;
+    }
+
+    public override Task Stop()
+    {
+        timer.Stop();
+        capture.StopRecording();
+
+        return Task.CompletedTask;
+    }
+
+    private void OnDataAvailableFP(object sender, WaveInEventArgs args)
+    {
+        float max = 0;
+        float[] buffer = new WaveBuffer(args.Buffer).FloatBuffer;
+
+        for (int index = 0; index < args.BytesRecorded / 4; index++)
+        {
+            var sample = Math.Abs(buffer[index]);
+
+            if (sample > max) max = sample;
+        }
+
+        stPeaks.Add(max);
+    }
+
+    private void OnDataAvailablePCM(object sender, WaveInEventArgs args)
+    {
+        float max = 0;
+        for (int i = 0; i < args.BytesRecorded; i += 3)
+        {
+            int sample = args.Buffer[i] | (args.Buffer[i + 1] << 8) | (args.Buffer[i + 2] << 16);
+
+            if ((sample & 0x800000) != 0) sample |= unchecked((int)0xFF000000);
+
+            sample = Math.Abs(sample);
+
+            if (sample > max) max = sample;
+        }
+
+        stPeaks.Add(max);
+    }
+}
+
+public sealed class WasapiVolumeProvider : VolumeProvider
+{
+    private readonly object capture;
+    private readonly bool _lb;
+
+    public WasapiVolumeProvider(double[] peaks, double[] avgs, float dbcorr, bool lb, MMDevice device, WaveFormat waveFormat)
+    {
+        _dbCorr = dbcorr;
+        _lb = lb;
+        ieeeFloat = waveFormat.Encoding.ToString() == "IeeeFloat";
+
+        window = Application.Current.MainWindow;
+        _peaks = peaks;
+        _avgs = avgs;
+        stPeaks = [];
+        timer = new() { Interval = 1000 };
+        timer.Tick += new EventHandler(Timer_Tick);
+
+        capture = _lb ? new WasapiLoopbackCapture(device) { WaveFormat = waveFormat } :
+            new WasapiCapture(device) { WaveFormat = waveFormat };
+
+        if (_lb) { (capture as WasapiLoopbackCapture).DataAvailable += ieeeFloat ? OnDataAvailableFP : OnDataAvailablePCM; }
+        else { (capture as WasapiCapture).DataAvailable += ieeeFloat ? OnDataAvailableFP : OnDataAvailablePCM; }
+    }
+
+    public override Task Start()
+    {
+        try
+        {
+            if (_lb) { (capture as WasapiLoopbackCapture).StartRecording(); }
+            else { (capture as WasapiCapture).StartRecording(); }
+        }
+        catch { MessageBox.Show("Error opening WASAPI stream. Check the sample rate and channels!", "Device Error", MessageBoxButton.OK, MessageBoxImage.Error); }
+
+        timer.Start();
+
+        return Task.CompletedTask;
+    }
+
+    public override Task Stop()
+    {
+        timer.Stop();
+
+        if (_lb) { (capture as WasapiLoopbackCapture).StopRecording(); }
+        else { (capture as WasapiCapture).StopRecording(); }
+
+        return Task.CompletedTask;
+    }
+
+    private void OnDataAvailableFP(object sender, WaveInEventArgs args)
+    {
+        float max = 0;
+        float[] buffer = new WaveBuffer(args.Buffer).FloatBuffer;
+
+        for (int index = 0; index < args.BytesRecorded / 4; index++)
+        {
+            var sample = Math.Abs(buffer[index]);
+
+            if (sample > max) max = sample;
+        }
+
+        stPeaks.Add(max);
+    }
+
+    private void OnDataAvailablePCM(object sender, WaveInEventArgs args)
+    {
+        float max = 0;
+        for (int i = 0; i < args.BytesRecorded; i += 3)
+        {
+            int sample = args.Buffer[i] | (args.Buffer[i + 1] << 8) | (args.Buffer[i + 2] << 16);
+
+            if ((sample & 0x800000) != 0) sample |= unchecked((int)0xFF000000);
+
+            sample = Math.Abs(sample);
+
+            if (sample > max) max = sample;
+        }
+
+        stPeaks.Add(max);
+    }
+}
+
+public sealed class AsioVolumeProvider : VolumeProvider
+{
+    private readonly AsioOut _device;
+
+    public AsioVolumeProvider(double[] peaks, double[] avgs, float dbcorr, AsioOut device, int samplerate)
+    {
+        ieeeFloat = true;
+
+        _device = device;
+        _dbCorr = dbcorr;
+
+        window = Application.Current.MainWindow;
+        _peaks = peaks;
+        _avgs = avgs;
+        stPeaks = [];
+        timer = new() { Interval = 1000 };
+        timer.Tick += new EventHandler(Timer_Tick);
+
+        _device.InitRecordAndPlayback(null, 1, samplerate);
+        _device.AudioAvailable += OnDataAvailable;
+    }
+
+    public override Task Start()
+    {
+        try
+        {
+            _device.Play();
+        }
+        catch { MessageBox.Show("Error opening ASIO device. Check the sample rate and channel!", "Device Error", MessageBoxButton.OK, MessageBoxImage.Error); }
+
+        timer.Start();
+
+        return Task.CompletedTask;
+    }
+
+    public override Task Stop()
+    {
+        timer.Stop();
+
+        _device.Stop();
+
+        return Task.CompletedTask;
+    }
+
+    private void OnDataAvailable(object sender, AsioAudioAvailableEventArgs args)
+    {
+        float[] buffer = args.GetAsInterleavedSamples();
+
+        stPeaks.Add(buffer.Select(Math.Abs).Max());
+    }
+}
---
+++ README.md
@@ -0,0 +1,105 @@
+﻿# Continuous Live Normalizer
+## About the application
+This application is developed in C# using .NET 10. It uses [NAudio](https://github.com/naudio/NAudio), [ScottPlot](https://github.com/ScottPlot/ScottPlot) and a [VBVMR-API wrapper for C#](https://github.com/A-tG/Voicemeeter-Remote-API-dll-dynamic-wrapper).
+
+The application is designed to calculate the volume of an audio source (WASAPI, WASAPI Loopback, WaveIn or ASIO) in dBfs (can be calibrated to match measured SPL) and automatically reduce the volume of a strip/bus in [Voicemeeter](https://voicemeeter.com/) when the input signal gets too loud over a set amount of time.
+
+## Usage
+After starting the application, select an audio API and device which will be used as the audio source. After adjusting the options, slick on "Start" to start the application.
+
+The "Restart" button will "press" the "Stop" button, wait three seconds and press the "Start" button while also restarting the connection to the VBVMR-API.
+
+## Options
+<details>
+<summary>API, Device, Channel</summary>
+
+	Accepts: Selection	Default: -
+
+	Selects the audio API and Device to use. 
+	Channel refers to ASIO channels and is only available when selecting it as the API.
+</details>
+
+<details>
+<summary>Use 32-bit FP</summary>
+
+	Accepts: Boolean	Default: False
+
+	Selects if 32-bit floating point audio should be used. 
+	If disabled 24-bit PCM will be used.
+</details>
+
+<details>
+<summary>Sample rate</summary>
+
+	Accepts: Integer	Default: 48
+
+	Sets the sample rate in kHz used for the Application (will also work when set SR is lower than the SR of the device).
+</details>
+
+<details>
+<summary>Channel count</summary>
+
+	Accepts: Integer	Default: 2
+
+	Sets the number of channels the device has.
+
+	Attention!
+	The application will expect Mono(-ish) audio so this will be used to skip samples.
+	For example, setting channel count to 2 will only use samples from the left channel and ignore the right channel to save processing power for mirrored signals.
+	If all channels need to be used for the volume calculation, set this to 1 so that no sample gets skipped.
+</details>
+
+<details>
+<summary>Averaging time</summary>
+
+	Accepts: Integer	Default: 10
+
+	Sets the time, in which the average will be calculated.
+	This also dictates the interval at which the application will check if the average is higher than the threshold.
+</details>
+
+<details>
+<summary>dB calibration</summary>
+
+	Accepts: Float		Default: 0
+
+	Sets the value that will be added to the calculated dB to match measured SPL.
+</details>
+
+<details>
+<summary>Bus/Strip selection</summary>
+
+	Accepts: Integer	Default: Bus 0
+
+	Pressing the "Bus/Strip" button will switch between Bus (Voicemeeter output strips) and Strip (Voicemeeter input strips).
+	The number dictates what Bus/Strip to use (0-7 for Voicemeeter Potato)
+</details>
+
+<details>
+<summary>Threshold</summary>
+
+	Accepts: Float		Default: -6
+
+	Sets the threshold at which the application will reduce the volume, if the average exceeds it.
+</details>
+
+<details>
+<summary>Return to</summary>
+
+	Accepts: Float*		Default: -12
+
+	Sets the level to which the application will reduce the volume to.
+
+	Note:
+	Its advised to keep this a bit under the threshold to prevent micro adjustments becuase of minor fluctuations.
+
+	*Although the value is read as a float and used to calculate the distance to the last average, this distance will be cast to an Integer.
+</details>
+
+<details>
+<summary>Fade rate</summary>
+
+	Accepts: Integer	Default: 10
+
+	Dictates the speed at rate the volume will decrease.
+</details>
